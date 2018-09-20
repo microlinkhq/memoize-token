741ed769045ccae7598c76375aaffadbc67f6998
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-09-20 09:29:52 +0200

    refactor: reduce number of cache calls

--- index.js
+++ index.js
@@ -9,32 +9,19 @@ module.exports = (fn, { key, max, cache = new Map(), expire } = {}) => {
   assert(max, 'max required')
   assert(key, 'key required')
 
-  const keyValue = `${key}:value`
-  const keyCount = `${key}:count`
-  const keyTimestamp = `${key}:timestamp`
-
   return async (...args) => {
-    let [value, count = 0, timestamp] = await Promise.all([
-      cache.get(keyValue),
-      cache.get(keyCount),
-      cache.get(keyTimestamp)
-    ])
-
+    let { value, count = 0, timestamp } = (await cache.get(key)) || {}
     const now = Date.now()
-    const isExpired = isNil(expire) ? false : now > timestamp
 
-    if (isNil(value) || isExpired || count >= max) {
+    if (isNil(value) || (isNil(expire) && now > timestamp) || count >= max) {
       value = await fn(args)
-      count = 0
+      count = 1
       timestamp = now + expire
-      await Promise.all([
-        cache.set(keyValue, value),
-        cache.set(keyCount, count),
-        cache.set(keyTimestamp, timestamp)
-      ])
+      await cache.set(key, { value, count, timestamp })
+      return value
     }
 
-    await cache.set(keyCount, count + 1)
+    await cache.set(key, { value, count: count + 1, timestamp })
     return value
   }
 }
--- test.js
+++ test.js
@@ -7,56 +7,60 @@ const memoizeToken = require('.')
 
 test('successive calls', async t => {
   const cache = new Map()
+  const key = 'test'
   let value = -1
   let values = ['foo', 'bar']
-  const fn = memoizeToken(() => values[++value], { max: 2, cache, key: 'test' })
+  const fn = memoizeToken(() => values[++value], { max: 2, cache, key })
 
   await fn()
   await fn()
-  t.is(cache.get('test:value'), 'foo')
-  t.is(cache.get('test:count'), 2)
+
+  t.is(cache.get(key).value, 'foo')
+  t.is(cache.get(key).count, 2)
 })
 
 test('token refresh', async t => {
   const cache = new Map()
+  const key = 'test'
   let value = -1
   let values = ['foo', 'bar']
-  const fn = memoizeToken(() => values[++value], { max: 2, cache, key: 'test' })
+  const fn = memoizeToken(() => values[++value], { max: 2, cache, key })
 
   await fn()
   await fn()
-  t.is(cache.get('test:value'), 'foo')
-  t.is(cache.get('test:count'), 2)
+  t.is(cache.get(key).value, 'foo')
+  t.is(cache.get(key).count, 2)
   await fn()
   await fn()
-  t.is(cache.get('test:value'), 'bar')
-  t.is(cache.get('test:count'), 2)
+  t.is(cache.get(key).value, 'bar')
+  t.is(cache.get(key).count, 2)
 })
 
 test('expire time', async t => {
   const cache = new Map()
+  const key = 'test'
   let value = -1
   let values = ['foo', 'bar', 'baz']
   const fn = memoizeToken(() => values[++value], {
     max: 2,
     cache,
-    key: 'test',
+    key,
     expire: 100
   })
 
   await fn()
-  t.is(cache.get('test:value'), 'foo')
-  t.is(cache.get('test:count'), 1)
+  t.is(cache.get(key).value, 'foo')
+  t.is(cache.get(key).count, 1)
   await fn()
-  t.is(cache.get('test:value'), 'foo')
-  t.is(cache.get('test:count'), 2)
+  t.is(cache.get(key).value, 'foo')
+  t.is(cache.get(key).count, 2)
   await delay(100)
   await fn()
-  t.is(cache.get('test:value'), 'bar')
-  t.is(cache.get('test:count'), 1)
+  t.is(cache.get(key).value, 'bar')
+  t.is(cache.get(key).count, 1)
   await fn()
   await fn()
   await fn()
-  t.is(cache.get('test:value'), 'baz')
-  t.is(cache.get('test:count'), 2)
+  t.is(cache.get(key).value, 'baz')
+  t.is(cache.get(key).count, 2)
 })
